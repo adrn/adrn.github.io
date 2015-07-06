1b14ec30cf432f9c0b5d0d291dff6c747737da22
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-07-05 23:52:51 -0400

    configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+import os
 
 AUTHOR = u'adrn'
-SITENAME = u'kadath in the cold waste'
+SITENAME = u'in the cold waste'
+SITESUBTITLE = u'<a href="http://adrian.pw">adrn</a>'
 SITEURL = ''
 
 PATH = 'content'
+PELICAN_STUFF_PATH = os.path.expanduser("~/projects/pelican-stuff")
 
 TIMEZONE = 'America/New_York'
 
@@ -19,17 +22,38 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Github', 'https://github.com/adrn'),
+          ('Twitter', 'https://twitter.com/adrianprw'),
+          ('Linkedin', 'https://www.linkedin.com/profile/view?id=318215201&trk=nav_responsive_tab_profile_pic'))
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+# Formatting for dates
+DEFAULT_DATE_FORMAT = ('%a %d %B %Y')
+
+# Formatting for urls
+ARTICLE_URL = "posts/{date:%Y}/{date:%m}/{slug}/"
+ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%m}/{slug}/index.html"
+
+CATEGORY_URL = "category/{slug}"
+CATEGORY_SAVE_AS = "category/{slug}/index.html"
+
+TAG_URL = "tag/{slug}/"
+TAG_SAVE_AS = "tag/{slug}/index.html"
+
+# Generate yearly archive
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+
+# Themes
+THEME = os.path.join(PELICAN_STUFF_PATH, 'pelican-themes', 'aboutwilson')
+
+# Plugins
+PLUGIN_PATHs = [os.path.join(PELICAN_STUFF_PATH, 'pelican-plugins')]
+PLUGINS = ['latex', 'neighbors', 'summary']
+
+# Only use LaTeX for selected articles
+LATEX = 'article'
