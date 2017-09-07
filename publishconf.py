c7f41d4472dc7735f959eb4ea7cf0b3a132de492
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-09-06 21:01:48 -0400

    update plugins

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://blog.adrian.pw'
+SITEURL = 'http://adrian.pw'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
